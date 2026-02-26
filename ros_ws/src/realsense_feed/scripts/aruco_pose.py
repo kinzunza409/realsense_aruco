c5b7b1d594f2883f0e6d6613fe260868be26ebc5
Kyle <inzukyle@gmail.com>
2026-02-26 16:01:20 -0500

    Added transforms so all arcuo poses are wrt the pose of a specific aruco

---
+++ ros_ws/src/realsense_feed/scripts/aruco_pose.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+import numpy as np
+
+import rospy
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge
+import tf2_ros
+import tf2_msgs.msg
+import tf.transformations
+import geometry_msgs.msg
+
+import cv2 as cv
+
+IDENTITY_TRANSFORM = geometry_msgs.msg.TransformStamped()
+IDENTITY_TRANSFORM.transform.rotation.w = 1.0
+
+def pose_distance(pose1, pose2):
+    p1 = np.array([pose1.position.x, pose1.position.y, pose1.position.z])
+    p2 = np.array([pose2.position.x, pose2.position.y, pose2.position.z])
+    return np.linalg.norm(p2 - p1)
+
+# converts OpenCV translatation and rotation vectors to a ROS Pose
+def cv_to_pose(r, t):
+
+    t = t.flatten()
+    r = r.flatten()
+    
+    angle = np.linalg.norm(r)
+    axis = r / angle
+    q = tf.transformations.quaternion_about_axis(angle, axis)
+    
+    p = geometry_msgs.msg.Pose()
+    p.position.x = t[0]
+    p.position.y = t[1]
+    p.position.z = t[2]
+    p.orientation.x = q[0]
+    p.orientation.y = q[1]
+    p.orientation.z = q[2]
+    p.orientation.w = q[3]
+
+    return p
+
+# returns translation distance between two transforms in mm
+def transform_translation_distance(t1, t2):
+    
+    # extract the transform from the transfromstamped
+    t1 = t1.transform
+    t2 = t2.transform
+    
+    p1 = np.array([t1.translation.x, t1.translation.y, t1.translation.z])
+    p2 = np.array([t2.translation.x, t2.translation.y, t2.translation.z])
+    return np.linalg.norm(p2 - p1) * 1000
+
+def transform_pose(pose, transform):
+    # build 4x4 matrix from transform
+    T = tf.transformations.quaternion_matrix([
+        transform.rotation.x,
+        transform.rotation.y,
+        transform.rotation.z,
+        transform.rotation.w
+    ])
+    T[0][3] = transform.translation.x
+    T[1][3] = transform.translation.y
+    T[2][3] = transform.translation.z
+
+    # build 4x4 matrix from pose
+    P = tf.transformations.quaternion_matrix([
+        pose.orientation.x,
+        pose.orientation.y,
+        pose.orientation.z,
+        pose.orientation.w
+    ])
+    P[0][3] = pose.position.x
+    P[1][3] = pose.position.y
+    P[2][3] = pose.position.z
+
+    # multiply
+    result_matrix = np.dot(T, P)
+
+    # extract result back into a Pose
+    result = geometry_msgs.msg.Pose()
+    result.position.x = result_matrix[0][3]
+    result.position.y = result_matrix[1][3]
+    result.position.z = result_matrix[2][3]
+    q = tf.transformations.quaternion_from_matrix(result_matrix)
+    result.orientation.x = q[0]
+    result.orientation.y = q[1]
+    result.orientation.z = q[2]
+    result.orientation.w = q[3]
+    return result
+
+def apply_transforms(p, transforms):
+    result = p
+    for transform in transforms:
+        t = transform.transform if isinstance(transform, geometry_msgs.msg.TransformStamped) else transform
+        result = transform_pose(result, t)
+    return result
+
+# find transform from o1 to o2
+def find_transform(p, q, id = None):
+    # assume that p and q are in o1 frame of reference
+    
+    T = geometry_msgs.msg.TransformStamped()
+    
+    T.header.frame_id = id
+    T.header.stamp = rospy.Time.now()
+
+    T.transform.translation = p
+    T.transform.rotation = q
+
+    return T
+
+def main():
+    rospy.init_node('realsense_aruco_pose')
+    rospy.loginfo("RealSense Aruco Pose Node Started")
+    cvbridge = CvBridge()
+
+    # get camera intrinsics
+    camera_info = rospy.wait_for_message('/camera/color/camera_info', CameraInfo)
+    camera_k = np.array(camera_info.K).reshape(3, 3)  # 3x3 intrinsic matrix as flat list of 9 values
+    camera_d = np.array(camera_info.D)  # distortion coefficients
+
+    # aruco detection initializations
+    aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_1000)
+    aruco_params = cv.aruco.DetectorParameters_create()
+    marker_length = 0.04 # side length in meters
+    origin_id = 222 # aruco id for marking robot base
+
+    # frame transforms
+    T_ca = IDENTITY_TRANSFORM # tranform between camera and aruco
+    T_ar = IDENTITY_TRANSFORM # transform between aruco and robot base
+
+    def image_callback(msg):
+        nonlocal T_ca, T_ar
+        
+        frame = cvbridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+        # aruco detection
+        # TODO: try this with the OpenCV 4.8 params to see if it performs better
+        corners, ids, rejected = cv.aruco.detectMarkers(frame, aruco_dict, parameters=aruco_params)
+        rvecs, tvecs, _objPoints = cv.aruco.estimatePoseSingleMarkers(corners, marker_length, camera_k, camera_d)
+
+        
+
+        if ids is not None:
+            
+            # convert vectors to poses
+            tag_poses = [cv_to_pose(r,t) for r,t in zip(rvecs, tvecs)]
+
+            rospy.loginfo_throttle(5.0, "\n".join([f"Found {len(ids)} markers"] + [f"Marker {id[0]}: ({pose.position.x*1000:.1f}mm, {pose.position.y*1000:.1f}mm, {pose.position.z*1000:.1f}mm)" for id, pose in zip(ids, tag_poses)]))
+            
+            # check if origin ID found
+            if origin_id in ids:
+                idx = list(ids.flatten()).index(222)
+                origin_c = tag_poses[idx] # pose of tag wrt camera
+                T_temp = find_transform(origin_c.position, origin_c.orientation)
+
+                # check if origin aruco has moved
+                if transform_translation_distance(T_temp, T_ca) > 10:
+                    # update transform
+                    T_ca = T_temp
+                    rospy.logwarn(f"Origin Marker ID {origin_id} has moved")
+            else:
+                rospy.logwarn_throttle(5*60,f"Origin Marker ID {origin_id} is obscured")
+
+            #transform poses to robot base frame of reference
+            tag_poses_transformed = [apply_transforms(p, [T_ca, T_ar]) for p in tag_poses]
+            
+
+            #cv.aruco.drawDetectedMarkers(frame, corners, ids)
+            for r,t in zip(rvecs, tvecs):
+                frame = cv.aruco.drawAxis(frame, camera_k, camera_d, r, t, marker_length)
+                if len(tag_poses) >= 2:
+                    id_222 = list(ids.flatten()).index(222)
+                    id_333 = list(ids.flatten()).index(333)
+                    pose_333 = tag_poses_transformed[id_333]
+                    x = pose_333.position.x
+                    y = pose_333.position.y
+                    z = pose_333.position.z
+                    cv.putText(frame, f"({x:.3f}, {y:.3f}, {z:.3f})", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
+
+                #cv.putText(frame, f"({x:.3f}, {y:.3f}, {z:.3f})", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
+                #cv.putText(frame, f"(DIST: {dist:.2f}mm)", (50, 100), cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+        else:
+            cv.putText(frame, 'NO MARKERS DETECTED!', (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
+
+        cv.imshow("RealSense Feed", frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            cv.destroyAllWindows()
+            rospy.signal_shutdown("User quit")
+
+    rospy.Subscriber('/camera/color/image_raw', Image, image_callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
